33b49d7e99c7a67be5e96f04be3906cdb6bbe6ea
Pavel <43359673+Warasar@users.noreply.github.com>
2020-05-22 14:59:10 +0900

    Add files via upload

---
+++ iz1.4/iz1.4.cpp
@@ -0,0 +1,22 @@
+﻿/* Напишите функцию factorial(n),возвращающую факториал целоко неотрицательного числа n.
+Напишите с использованием этой функции программу,которая вводит неотрицательные целые числа n,k(k⩽n), 
+и выводит значение числа сочетаний по k из n элементов: Ck n= n! k!(n−k)!Пример ввода: 5 3 Правильный вывод:10 */
+#include <iostream>
+using namespace std;
+
+float factorial(unsigned n) 
+{
+	long long p = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		p = p * i;
+	}
+	return p;
+}
+int main() 
+{
+	unsigned N, k;
+	cin >> N;
+	cin >> k;
+	cout << factorial(N) / (factorial(k) * factorial(N - k)) << endl;
+}
---
+++ iz2.1/iz2.1.cpp
@@ -0,0 +1,29 @@
+﻿/* Напишите функцию angle(x,y,z), которая дает угол, противолежащий стороне z в треугольнике состоронами x,y,z.
+Напишите с использованием этой функции программу,которая вводит вещественные длины 
+трех сторон треугольника и выводит углы,противолежащие сторонам в том же порядке, 
+в градусах c фиксированной точностью 1 знак после десятичной точки. Примерввода:1 1 1.4142 Правильныйвывод:45.0 45.0 90.0*/
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+double power(double m) 
+{
+	return m * m;
+}
+
+float angle(double x, double y, double z) 
+{
+	double p;
+	p = acos((power(x) + power(y) - power(z)) / (2 * x * y));
+	return p * 180.0 / M_PI;
+}
+int main() 
+{
+	float a, b, c;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cout << fixed << setprecision(1) << angle(c, b, a) << " " << angle(a, c, b) << " " << angle(a, b, c) << endl;
+}
